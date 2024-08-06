0faab20b1a1dcc705c044195cfeb36f0e455451b
Luis Neto <luis.adsn@outlook.com>
2024-08-06 19:49:24 -0300

    Exclusão de lixo.
    Arquivos desnecessários e/ou indevidos.

--- automacao_selenium.py
+++
@@ -1,5 +0,0 @@
-# Importando webdriver da biblioteca selenium
-from selenium import webdriver
-
-# Abrindo o navegador para começar a automacação, utilizando o webdriver
-chrome = webdriver.Chrome()
